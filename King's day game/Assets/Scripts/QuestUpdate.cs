0d19e6d89aca7cfa6fc397744d280d94954423cc
Juan Pablo <73938967+YTBEAR@users.noreply.github.com>
2025-06-10 02:55:01 +0200

    necessary changes

--- King's day game/Assets/Scripts/QuestUpdate.cs
+++ King's day game/Assets/Scripts/QuestUpdate.cs
@@ -6,19 +6,20 @@ public class QuestUpdate : MonoBehaviour
     public TextMeshProUGUI task4Hint;
     Tasks tasks;
     QuestGiver questGiver;
-    public bool questTriggered;
     private void Start()
     {
         tasks = FindFirstObjectByType<Tasks>();
         questGiver = FindFirstObjectByType<QuestGiver>();
-        questTriggered = false;
     }
     private void OnDestroy()
     {
         if (!Application.isPlaying || TaskManager.quest4Complete == true) return;
 
-        task4Hint.text = $"Current objective: Give the present to the guy";
-        questGiver.QuestUpdate();
-        questGiver.cost = GetComponent<Value>().value;
+        if (TaskManager.quest4Found)
+        {
+            task4Hint.text = $"Current objective: Give the present to the guy";
+            questGiver.QuestUpdate();
+            questGiver.cost = GetComponent<Value>().value;
+        }
     }
 }
